e9e21ad871715540ec403fcbb10133a3b4258303
Tales <jobbtales@gmail.com>
2023-06-09 00:31:21 -0300

    some changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,23 @@
+import './globals.css'
+
+import { Toaster } from 'react-hot-toast'
+import { SessionProvider } from 'next-auth/react'
+
 import type { AppProps } from 'next/app'
 
-import './globals.css'
 import Layout from '@/components/Layout'
+import LoginModal from '@/components/modals/LoginModal'
+import RegisterModal from '@/components/modals/RegisterModal'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <SessionProvider session={pageProps.session}>
+      <Toaster />
+      <RegisterModal />
+      <LoginModal />
       <Layout>
         <Component {...pageProps} />
       </Layout>
-    </>
+    </SessionProvider>
   )
 }
